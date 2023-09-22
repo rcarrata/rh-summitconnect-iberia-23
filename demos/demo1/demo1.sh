5a3b8ada5e6e964b8cb222c071f1638505538459
rcarrata <rcarratalasanchez@gmail.com>
2023-09-22 17:41:56 +0200

    minor fixes for demo1

--- demos/demo1/demo1.sh
+++ demos/demo1/demo1.sh
@@ -2,36 +2,27 @@ source ../utils/demo-magic.sh
 
 pei "# Deploying Demo 1 - Cloud Services ACM Submariner"
 pe "export KUBECONFIG=/var/tmp/acm-lab-kubeconfig"
-pe "export ROSA_CLUSTER_NAME='rosa-rcarrata'" 
-pe "export ARO_CLUSTER='aro-rcarrata'"
-pei ""
-
-pei "# Clone Apps Demo Repository"
-pe "git clone https://github.com/rh-mobb/acm-demo-app /tmp/acm-demo-app && cd /tmp/acm-demo-app"
+pe "export ROSA_CLUSTER_NAME='rosa-summit'" 
+pe "export ARO_CLUSTER='aro-summit'"
 pei ""
 
 pei "# Deploy GuestBook FrontEnd"
-pe "kubectl config use hub && oc apply -k guestbook-app/acm-resources"
-pei ""
-
-pei "# Deploy the Redis Master App in ROSA Cluster"
-pe "oc apply -k redis-master-app/acm-resources"
-pei ""
-
-pei "# Deploy the Redis Master App in ROSA Cluster"
-pe "kubectl config use $ROSA_CLUSTER_NAME"
-pe "oc adm policy add-scc-to-user anyuid -z default -n guestbook"
-pe "oc delete pod --all -n guestbook"
-pei ""
-
-pei "# Deploy the Redis Slave App in ROSA Cluster 2"
+pe ""
 pe "kubectl config use hub"
-pe "oc apply -k redis-slave-app/acm-resources"
+pe "oc apply -k ../../apps/guestbook-app/acm-resources"
 pei ""
 
+pei "# Deploy the Redis Master App in ROSA Cluster"
+pe "oc apply -k ../../apps/redis-master-app/acm-resources"
+pei ""
 
-pei "# Apply relaxed SCC only for this PoC"
-pe "kubectl config use $ROSA_CLUSTER_NAME_2"
+pei "# Adjust the SCCs in ROSA"
+pe "kubectl config use $ARO_CLUSTER_NAME"
 pe "oc adm policy add-scc-to-user anyuid -z default -n guestbook"
 pe "oc delete pod --all -n guestbook"
+pei ""
+
+pei "# Deploy the Redis Slave App in ARO Cluster"
+pe "kubectl config use hub"
+pe "oc apply -k ../../apps/redis-slave-app/acm-resources"
 pei ""
